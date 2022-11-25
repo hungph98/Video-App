2a8bca3bbfa9997801236de9cc70dbcaa28894e7
hungph98 <hungph62@wru.vn>
2022-11-25 15:22:14 +0700

    update code backend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,13 @@
-import express from "express";
-import dotenv from "dotenv";
-import mongoose from "mongoose";
-
-
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv")
 const app = express();
+
+const userRoutes = require("./src/routes/users");
+const videoRoutes = require("./src/routes/videos");
+const commentRoutes = require("./src/routes/comments");
+const authRoutes = require("./src/routes/auth");
+const cookieParser = require("cookie-parser");
 
 dotenv.config();
 
@@ -15,4 +19,23 @@ const connect = async () => {
     }) 
 }
 
-app.listen(process.env.PORT,() => {console.log("App working ...."), connect()})
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/videos", videoRoutes);
+app.use("/api/comments", commentRoutes);
+
+
+app.use((err, req, res, next) => {
+    const status = err.status || 500;
+    const message = err.message || "Something went wrong!!";
+
+    return res.status(status).json({
+        success: false,
+        status,
+        message,
+    })
+})
+
+app.listen(process.env.PORT,() => {console.log(`App working .... http://localhost:${process.env.PORT}`), connect()})
---
+++ backend/src/controllers/userController.js
@@ -0,0 +1,35 @@
+const User = require("../models/Users")
+
+class UserController {
+
+    updateUser = async(req, res, next) => {
+
+    }
+
+    deleteUser = async(req, res, next) => {
+        
+    }
+
+    getUser = async(req, res, next) => {
+        
+    }
+
+    subscribeUser = async(req, res, next) => {
+        
+    }
+
+    unsubscribeUser = async(req, res, next) => {
+        
+    }
+
+    likeUser = async(req, res, next) => {
+        
+    }
+
+    dislikeUser = async(req, res, next) => {
+        
+    }
+    
+}
+
+module.exports = new UserController;
